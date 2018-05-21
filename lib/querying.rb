d5364f2f668ec006e56b167baba8b81a32cb8153
Pamela Frieze <pfrieze@gmail.com>
2018-05-21 14:01:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON books.series.id = series.id JOIN character_books ON character_books.id = books.id JOIN characters ORDER BY LENGTH(characters.species) DESC LIMIT 1;"
+  "SELECT series.title FROM series JOIN books ON books.series.id = series.id JOIN character_books ON character_books.id = books.id JOIN characters ON character_books.character_id ORDER BY LENGTH(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
