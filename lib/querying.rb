858055468de7e4257ee00e65e28a4f88d377936a
Pamela Frieze <pfrieze@gmail.com>
2018-05-21 13:33:54 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT subgenres.name, series.id FROM subgenres JOIN series;"
+  "SELECT subgenres.name, series.id FROM subgenres JOIN series ORDER BY subgenres.name;"
 end
 
 def select_series_title_with_most_human_characters
